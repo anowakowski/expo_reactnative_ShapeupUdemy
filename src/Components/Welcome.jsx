9137e64f08f02c74b9f4beda15e6cc92d0aeac66
anowakowski <nowakowski.tools@gmail.com>
2024-09-23 19:42:56 +0200

    added custom fonts

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,10 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useFonts } from 'expo-font';
 
 const Welcome = () => {
+  const [loaded, error] = useFonts({
+    Kameron: require('../../assets/fonts/Kameron-Bold.ttf'),
+  });
+  
+  if (!loaded){
+    return null;
+  }
+
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <Text style={{
+        fontFamily: "Kameron",
+        fontSize: 27,
+        textAlign: "center",
+        color: '#92400e'
+        }}>Welcome</Text>
     </View>
   );
 }
