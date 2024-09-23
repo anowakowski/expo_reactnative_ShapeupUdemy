6dc72ade464e39e89c27cd1209a2b8e4a4e87a7b
anowakowski <nowakowski.tools@gmail.com>
2024-09-23 15:25:59 +0200

    added base tabs  for TabNavigator, added some packages

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 
+import WorkoutScreen from './src/Screens/WorkoutScreen';
+import TimerScreen from './src/Screens/TimerScreen';
+import CalculationScreen from './src/Screens/CalculationScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
 export default function App() {
+
+  function TabNavigator() {
+    return(
+      <Tab.Navigator>
+        <Tab.Screen name='Workout' component={WorkoutScreen} />
+        <Tab.Screen name='Timer' component={TimerScreen} />
+        <Tab.Screen name='Calculation' component={CalculationScreen} />
+      </Tab.Navigator>
+    );
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='TabNavigator' component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
