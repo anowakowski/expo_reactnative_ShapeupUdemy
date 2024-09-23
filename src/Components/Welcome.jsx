f4a5726d229fbd40f77e3d08d01e8a97717b88c5
anowakowski <nowakowski.tools@gmail.com>
2024-09-23 19:54:45 +0200

    added next font packages

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -1,8 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useFonts } from 'expo-font';
+//import { useFonts } from 'expo-font';
+import {
+    useFonts,
+    Caveat_400Regular,
+    Caveat_500Medium,
+    Caveat_600SemiBold,
+    Caveat_700Bold,
+  } from '@expo-google-fonts/caveat';
 
 const Welcome = () => {
+  /*  
   const [loaded, error] = useFonts({
     Kameron: require('../../assets/fonts/Kameron-Bold.ttf'),
   });
@@ -10,13 +18,27 @@ const Welcome = () => {
   if (!loaded){
     return null;
   }
+  */
+  let [fontsLoaded] = useFonts({
+    Caveat_400Regular,
+    Caveat_500Medium,
+    Caveat_600SemiBold,
+    Caveat_700Bold,
+  });
 
+  if (!fontsLoaded){
+    return null;
+  }
+
+  let fontSize = 30;
+  let paddingVertical = 6;  
 
   return (
     <View>
       <Text style={{
-        fontFamily: "Kameron",
-        fontSize: 27,
+        //fontFamily: "Kameron",
+        fontFamily: 'Caveat_400Regular',
+        fontSize: fontSize,
         textAlign: "center",
         color: '#92400e'
         }}>Welcome</Text>
