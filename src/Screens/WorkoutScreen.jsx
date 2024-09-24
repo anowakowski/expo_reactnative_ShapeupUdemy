6f03bd5efe086d9869e6be413e4506c3466038f6
anowakowski <nowakowski.tools@gmail.com>
2024-09-24 13:36:45 +0200

    added Category, CategoryItems components

--- src/Screens/WorkoutScreen.jsx
+++ src/Screens/WorkoutScreen.jsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Welcome from '../Components/Welcome';
 import WorkoutOTD from '../Components/WorkoutOTD';
 import Separator from '../Components/Separator';
+import Category from '../Components/Category';
 
 const WorkoutScreen = () => {
   return (
@@ -11,6 +12,7 @@ const WorkoutScreen = () => {
       <Welcome />
       <WorkoutOTD />
       <Separator />
+      <Category />
     </SafeAreaView>
   );
 }
