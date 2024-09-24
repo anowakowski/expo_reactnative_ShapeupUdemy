2aee0480bdb5a0cb0066162f3b084383a1e09e67
anowakowski <nowakowski.tools@gmail.com>
2024-09-24 12:38:54 +0200

    added separator

--- src/Screens/WorkoutScreen.jsx
+++ src/Screens/WorkoutScreen.jsx
@@ -3,12 +3,14 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Welcome from '../Components/Welcome';
 import WorkoutOTD from '../Components/WorkoutOTD';
+import Separator from '../Components/Separator';
 
 const WorkoutScreen = () => {
   return (
     <SafeAreaView className='mx-[1%]'>
       <Welcome />
       <WorkoutOTD />
+      <Separator />
     </SafeAreaView>
   );
 }
