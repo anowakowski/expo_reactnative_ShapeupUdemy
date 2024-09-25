78396dfcabe4e975ea4561c4e3a028427a81427c
anowakowski <nowakowski.tools@gmail.com>
2024-09-25 15:51:44 +0200

    added flash-list, expo-dev-client, firebase

---
+++ Firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDUY67JUuISBk709CZwiEIjkasZh5iLMMQ",
+  authDomain: "shapeupudemy-71b78.firebaseapp.com",
+  projectId: "shapeupudemy-71b78",
+  storageBucket: "shapeupudemy-71b78.appspot.com",
+  messagingSenderId: "741107358196",
+  appId: "1:741107358196:web:00391f34f70857326881a1",
+  measurementId: "G-7N90ME5180"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/Screens/WorkoutScreen.jsx
+++ src/Screens/WorkoutScreen.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import Welcome from '../Components/Welcome';
-import WorkoutOTD from '../Components/WorkoutOTD';
-import Separator from '../Components/Separator';
-import Category from '../Components/Category';
-import Exercise from '../Components/Exercise';
+import Welcome from "../Components/Welcome";
+import WorkoutOTD from "../Components/WorkoutOTD";
+import Separator from "../Components/Separator";
+import Category from "../Components/Category";
+import Exercise from "../Components/Exercise";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+} from "react-native-gesture-handler";
 
 const WorkoutScreen = () => {
   return (
-    <SafeAreaView className='mx-[1%]'>
-      <Welcome />
-      <WorkoutOTD />
-      <Separator />
-      <Category />
-      <Separator />
-      <Exercise />
-    </SafeAreaView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ScrollView>
+        <SafeAreaView className="mx-[1%]">
+          <Welcome />
+          <WorkoutOTD />
+          <Separator />
+          <Category />
+          <Separator />
+          <Exercise />
+        </SafeAreaView>
+      </ScrollView>
+    </GestureHandlerRootView>
   );
-}
+};
 
 export default WorkoutScreen;
-
